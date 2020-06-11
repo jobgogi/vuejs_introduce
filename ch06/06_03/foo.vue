1210dd62d68313cc702f00e11c63d03467b004cb
jobgogi <ixymori@gmail.com>
2020-06-11 20:35:47 +0900

    scope css

---
+++ ch06/06_03/foo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="foo">
+    <h1 class="header">Foo 컴포넌트</h1>
+    <p>이것은 Foo 컴포넌트 입니다.</p>
+  </div>
+</template>
+
+<style>
+  p { text-decoration: underline; }
+</style>
+
+<style scoped>
+.foo {
+  border: solid 1px green;
+  margin: 4px;
+  padding: 4px;
+}
+.header {
+  font-size: 150%;
+}
+</style>
---
+++ ch06/06_03/root.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="root">
+    <h1 class="header">Root 컴포넌트</h1>
+    <p>이것은 Root 컴포넌트 입니다.</p>
+    <foo />
+  </div>
+</template>
+
+<script>
+import Foo from './foo';
+
+export default {
+  components: {
+    Foo
+  }
+}
+</script>
+
+<style>
+#root {
+  border: solid 1px blue;
+  margin: 4px;
+  padding: 4px;
+}
+
+.header {
+  font-size: 200%;
+}
+</style>
