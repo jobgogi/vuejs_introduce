d0d47effe855b50d6e3b9ab52a0a1ec4ffc1e8ba
jobgogi <ixymori@gmail.com>
2020-06-11 21:00:58 +0900

    css module

---
+++ ch06/06_04/root.vue
@@ -0,0 +1,15 @@
+<template>
+  <p :class="$style.message">안녕하세요</p>
+</template>
+<script>
+export default {
+  created() {
+    console.log('css modules: $style', this.$style);
+  }
+}
+</script>
+<style module>
+.message {
+  color: #42B983;
+}
+</style>
