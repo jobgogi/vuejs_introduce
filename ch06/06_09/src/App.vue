0c8052f9c4ecedbe649a8339244445a9a5e72324
jobgogi <ixymori@gmail.com>
2020-06-13 20:22:11 +0900

    add labels

--- ch06/06_09/src/App.vue
+++ ch06/06_09/src/App.vue
@@ -6,10 +6,27 @@
         <input type="checkbox" :id="`task-${task.id}`" v-bind:checked="task.done"
           v-on:change="toggleTaskStatus(task)">
         <label :for="`task-${task.id}`">&nbsp;{{ task.name }}</label>
+        -
+        <span v-for="id in task.labelIds" v-bind:key="id">
+          {{ getLabelText(id) }}
+        </span>
       </li>
     </ul>
     <form v-on:submit.prevent="addTask">
       <input type="text" v-model="form.newTaskName" placeholder="새 태스크">
+    </form>
+
+    <h2>레이블 목록</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input type="checkbox" :id="`label-${label.id}`" v-bind:value="label.id"
+          v-model="form.newTaskLabelIds">
+        <label :for="`label-${label.id}`">&nbsp;{{ label.text }}</label>
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addLabel">
+      <input type="text" v-model="form.newLabelText" placeholder="새 레이블">
     </form>
   </div>
 </template>
@@ -18,26 +35,43 @@ export default {
   data() {
     return {
       form: {
-        newTaskName: ''
+        newTaskName: '',
+        newTaskLabelIds: [],
+        newLabelText: ''
       }
     }
   },
   computed: {
     tasks() {
       return this.$store.state.tasks;
+    },
+    labels() {
+      return this.$store.state.labels;
     }
   },
   methods: {
     addTask() {
       this.$store.commit('addTask', {
-        name: this.form.newTaskName
+        name: this.form.newTaskName,
+        labelIds: this.form.newTaskLabelIds
       });
       this.form.newTaskName = '';
+      this.form.newTaskLabelIds = [];
     },
     toggleTaskStatus(task) {
       this.$store.commit('toggleTaskStatus', {
         id: task.id
       });
+    },
+    addLabel() {
+      this.$store.commit('addLabel', {
+        text: this.form.newLabelText
+      });
+      this.newLabelText = '';
+    },
+    getLabelText(id) {
+      const label = this.labels.filter(label => label.id === id)[0];
+      return label ? label.text : '';
     }
   }
 }
--- ch06/06_09/src/store/index.js
+++ ch06/06_09/src/store/index.js
@@ -9,21 +9,39 @@ const store = new Vuex.Store({
       {
         id: 1,
         name: '우유 사기',
+        labelIds: [1, 2],
         done: false
       },
       {
         id: 2,
         name: 'Vue.js 관련 책 사기',
+        labelIds: [1, 3],
         done: true
       }
     ],
-    nextTaskId: 3
+    labels: [
+      {
+        id: 1,
+        text: '쇼핑'
+      },
+      {
+        id: 2,
+        text: '음료'
+      },
+      {
+        id: 3,
+        text: '책'
+      }
+    ],
+    nextTaskId: 3,
+    nextLabelId: 4,
   },
   mutations: {
-    addTask(state, { name }) {
+    addTask(state, { name, labelIds }) {
       state.tasks.push({
         id: state.nextTaskId,
         name,
+        labelIds,
         done: false
       });
 
@@ -35,6 +53,13 @@ const store = new Vuex.Store({
         .forEach(task => {
           task.done = !task.done
         });
+    },
+    addLabel(state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      });
+      state.nextLabelId++;
     }
   }
 });
