99f8da97c418b3d0d3effd07dd500a4d8d0d8d08
jobgogi <ixymori@gmail.com>
2020-06-15 05:24:28 +0900

    Store

--- ch07/07_02/src/App.vue
+++ ch07/07_02/src/App.vue
@@ -1,32 +1,41 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <h2>Global Store</h2>
+    <p>{{ count }}</p>
+    <button type="button"
+      @click="globalUpdate">
+      Global Update
+    </button>
+    <h2>Local Store - example</h2>
+    <p>{{ index }} x 3{{ triple }}</p>
+    <button type="button"
+      @click="multiplyByFive">
+      global x 5
+    </button>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      index: 1
+    };
+  },
+  computed: {
+    count() {
+      return this.$store.getters.double;
+    },
+    triple() {
+      return this.$store.getters['example/triple'];
+    }
+  },
+  methods: {
+    globalUpdate() {
+      this.$store.commit('update', ++this.index);
+    },
+    multiplyByFive() {
+      this.$store.dispatch('example/multiplyByFive');
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- ch07/07_02/src/store/index.js
+++ ch07/07_02/src/store/index.js
@@ -1,15 +1,58 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = {
   state: {
+    count: 1
+  },
+  getters: {
+    double: state => state.count + state.count
   },
   mutations: {
-  },
-  actions: {
+    update(state, payload) {
+      state.count = payload;
+    }
   },
   modules: {
+    example: {
+      namespaced: true,
+      state: {
+        value: 'Example'
+      },
+      getters: {
+        upper(state) {
+          return state.value.toUpperCase();
+        },
+        /**
+         * 전역 네임스페이스에 접근하는 방법
+         * @param {*} state       - 현재 네임스페이스의 state
+         * @param {*} getters     - 현재 네임스페이스의 getters
+         * @param {*} rootState   - 전역 네임스페이스의 state를 가져온다
+         * @param {*} rootGetters - 전역 네임스페이스의 getters를 가져온다.
+         */
+        triple(state, getters, rootState, rootGetters) {
+          return rootState.count + rootGetters.double;
+        }
+      },
+      mutations: {
+        update(state) {
+          state.value = 'Updated';
+        }
+      },
+      actions: {
+        update(ctx) {
+          ctx.commit('update');
+        },
+        multiplyByFive(ctx) {
+          // 전역 double 게터와 example 모듈의 triple 게터를 사용
+          const payload = ctx.rootGetters.double + ctx.getters.triple;
+          ctx.commit('update', payload, { root: true });
+        }
+      }
+    }
   }
-})
+};
+
+export default new Vuex.Store(store);
